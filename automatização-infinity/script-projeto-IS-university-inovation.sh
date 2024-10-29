ea0ef39898401033e6076feec9fe2f3885fa50a4
AlbaDr52 <pablo.santana@sptech.school>
2024-10-29 16:27:55 -0300

    subindo script de automação 1.5

--- automatização-infinity/script-projeto-IS-university-inovation.sh
+++ automatização-infinity/script-projeto-IS-university-inovation.sh
@@ -1,17 +1,20 @@
 sudo apt update
 sudo apt upgrade
-sudo apt install docker.io
-sudo apt install docker-compose
+sudo apt install docker.io -y
+sudo apt install docker-compose -y
+sudo apt install npm -y
 cd
-mkdir projeto
-cd projeto
+mkdir /etc/projeto
+cd /etc/projeto
 git clone https://github.com/Infinity-Solutions-INC/institucional-infinitySolutions.git
 git clone https://github.com/Infinity-Solutions-INC/infinity-solutions-DB.git
+git clone https://github.com/Infinity-Solutions-INC/infinity-javaConnection-comLog.git
 cd institucional-infinitySolutions
 npm install
 rm .env.dev .env
 cd -
 mv infinity-solutions-DB/script_infinitySolutions.sql .
+mv infinity-javaConnection-comLog/conexao-banco-de-dados-1.0-SNAPSHOT-jar-with-dependencies.jar .
 
 ################################Definição Compose######################################
 cat <<EOF > docker-compose.yml
@@ -26,6 +29,8 @@ services:
     volumes:
       - db_data:/var/lib/mysql
       - ./script_infinitySolutions.sql:/docker-entrypoint-initdb.d/script_infinitySolutions.sql
+    networks:
+      - app-network
 
   node-app:
     image: node:14
@@ -39,14 +44,25 @@ services:
     command: ["npm", "start"]
     depends_on:
       - mysql-app
+    networks:
+      - app-network
+
   java-app:
     build: .
+    depends_on:
+      - mysql-app
     volumes:
       - ./output:/app/output
-
+    networks:
+      - app-network
 
 volumes:
   db_data:
+
+networks:
+  app-network:
+    driver: bridge
+
 EOF
 ####################################################################
 ###################Definição dockerfile (java)######################
@@ -57,10 +73,10 @@ WORKDIR /app
 
 RUN apt update && apt install -y cron
 
-COPY TimeLogger.jar .
+COPY conexao-banco-de-dados-1.0-SNAPSHOT-jar-with-dependencies.jar .
 
-RUN echo "* * * * * /usr/local/openjdk-21/bin/java -jar /app/TimeLogger.jar &&  mv execution_log.txt /app/output/" > /etc/cron.d/app-cron
-RUN echo "* * * * * echo 'Cron job is running' >> /app/output/test.log" >> /etc/cron.d/app-cron
+
+RUN echo "* * * * * /usr/local/openjdk-21/bin/java -jar /app/conexao-banco-de-dados-1.0-SNAPSHOT-jar-with-dependencies.jar &&  cp arquivo.log /app/output/" > /etc/cron.d/app-cron
 RUN chmod 664 /etc/cron.d/app-cron
 
 RUN crontab /etc/cron.d/app-cron
@@ -72,10 +88,10 @@ EOF
 ############Definição variaveis de ambiente###############################
 echo "AMBIENTE_PROCESSO=desenvolvimento
 # Configurações de conexão com o banco de dados
-DB_HOST='localhost'
-DB_DATABASE='infinity-solutions'
+DB_HOST='mysql-app'
+DB_DATABASE='infinity_solutions'
 DB_USER='root'
-DB_PASSWORD='root-password'
+DB_PASSWORD='rootpassword'
 DB_PORT=3306
 
 # Configurações do servidor de aplicação
@@ -85,5 +101,6 @@ APP_HOST=localhost
 # importante: caso sua senha contenha caracteres especiais, insira-a entre 'aspas'
 " > .env.dev
 ########################################################################################
-docker-compose up -d
+docker-compose up -d --build
+
 
